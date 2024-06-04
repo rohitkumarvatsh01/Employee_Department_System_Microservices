66a6f0085c74469609089cc2dea4329faf0f0fed
rohitkumarvatsh01 <rohitkumarvatsh01@gmail.com>
2024-06-05 00:43:40 +0530

    Add Config Server

--- DepartmentService/src/main/java/com/department/service/DepartmentFeignClient.java
+++ DepartmentService/src/main/java/com/department/service/DepartmentFeignClient.java
@@ -1,7 +1,6 @@
 package com.department.service;
 
 import com.department.model.Department;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
